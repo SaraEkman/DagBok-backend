4d03d9ef1978eee6a042f9af81b391e7fd0395ee
sara <sara.ekmanMedieinstitutet@gmail.com>
2022-05-22 02:36:27 +0200

    fix cors error

--- routes/post-route.js
+++ routes/post-route.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const PostModel = require('../modules/post_model');
 const cors = require('cors')
-router.use(cors({
-    "origin": "*",
-    "methods": ["GET", "POST", "PUT","DELETE"]
-}))
-
+router.use(cors())
+// {
+//     "origin": "*",
+//         "methods": ["GET", "POST", "PUT", "DELETE"];
+// }
 
 router.get('/', async (req, res) => {
     const posts = await PostModel.find();
